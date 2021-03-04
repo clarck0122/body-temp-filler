7b82c816fe4930b20a3fcf3ada7874da49d37942
Clarck <clarck0122@gmail.com>
2021-03-04 23:42:20 +0800

    Get id and pw variable from Env

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ import os
 def main():
 	config = configparser.ConfigParser()
 	config.read('Startup.ini')
-	id_li = config['Common']['User'].split(",")
+	# id_li = config['Common']['User'].split(",")
+	id_li = os.environ['User'].split(",")
+	
 
 	objSys = User()
 	objSys.name = "clarck"
@@ -20,9 +22,11 @@ def main():
 	for user_id in id_li:
 		user = User()
 		user.id = user_id
-		user.name = config[user.id]['name']
-		user.pw = config[user.id]['pw']
-		user.email = config[user.id]['email']
+		# user.name = config[user.id]['name']
+		# user.pw = config[user.id]['pw']
+		user.name = os.environ[user.id + '_name']
+		user.pw = os.environ[user.id + '_pw']
+		# user.email = config[user.id]['email']
 		print("id={}, name={}, pw={}, email={}".format(user.id, user.name, user.pw, user.email) )
 		user_li.append(user)
 
